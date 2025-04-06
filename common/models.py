0cc8b175a4022d8fba074b1b69c0abce8424d6e8
Volodymyr Tkach <vovantkac5@gmail.com>
2025-04-06 22:08:43 +0300

    fix issue with user creation, update .dockerignore

--- common/models.py
+++ common/models.py
@@ -18,7 +18,7 @@ class CustomUserManager(BaseUserManager):
     for authentication instead of usernames.
     """
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """
         Create and save a user with the given email and password.
         """
@@ -29,7 +29,7 @@ class CustomUserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
