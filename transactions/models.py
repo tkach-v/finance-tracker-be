85c97900d1e993c6565e7a9cc9bd9d6112ee1cb6
Volodymyr Tkach <vovantkac5@gmail.com>
2025-05-11 22:16:28 +0300

    Add category, type, and date fields to Transaction model; implement balance adjustment on save and delete

--- transactions/models.py
+++ transactions/models.py
@@ -1,8 +1,15 @@
-from django.db import models
+from django.db import models, transaction
+from django.db.models import F
 
 from accounts.models import Account
+from categories.models import Category
 from common.models import TimeStampedModel
 from finance_tracker import settings
+
+
+class TransactionType(models.TextChoices):
+    INCOME = "income", "Income"
+    EXPENSE = "expense", "Expense"
 
 
 class Transaction(TimeStampedModel):
@@ -15,6 +22,12 @@ class Transaction(TimeStampedModel):
         related_name="transactions",
         help_text="Account associated with the transaction",
     )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name="transactions",
+        help_text="Category of the transaction",
+    )
     amount = models.DecimalField(
         max_digits=30,
         decimal_places=10,
@@ -22,7 +35,40 @@ class Transaction(TimeStampedModel):
         help_text="Amount of the transaction",
     )
     description = models.CharField(max_length=255, blank=True)
+    type = models.CharField(max_length=10, choices=TransactionType.choices)
+    date = models.DateField(help_text="Date of the transaction")
 
     class Meta:
         ordering = ["-created_at"]
         verbose_name_plural = "Transactions"
+
+    def save(self, *args, **kwargs):
+        # when creating or updating, change account balance
+        if self._state.adding:
+            delta = self.amount if self.type == TransactionType.INCOME else -self.amount
+        else:
+            old = Transaction.objects.get(pk=self.pk)
+            old_delta = (
+                old.amount if old.type == TransactionType.INCOME else -old.amount
+            )
+            new_delta = (
+                self.amount if self.type == TransactionType.INCOME else -self.amount
+            )
+            delta = new_delta - old_delta
+
+        super().save(*args, **kwargs)
+
+        if delta:
+            with transaction.atomic():
+                Account.objects.filter(pk=self.account.pk).update(
+                    balance=F("balance") + delta
+                )
+
+    def delete(self, *args, **kwargs):
+        # when deleting, revert original effect on account balance
+        delta = -self.amount if self.type == TransactionType.INCOME else self.amount
+        with transaction.atomic():
+            Account.objects.filter(pk=self.account.pk).update(
+                balance=F("balance") + delta
+            )
+            super().delete(*args, **kwargs)
